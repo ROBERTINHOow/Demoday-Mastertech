5d83f0d81d5f80994b780dc96a2cc36535714bab
LuanaT <luaninha1961@live.com>
2018-04-10 23:58:50 -0300

    Edição das paginas Home, Login e Conteudo

---
+++ taketrip-site/home/script.js
@@ -0,0 +1,27 @@
+
+function carregarLogin(){
+    axios.get('http://localhost:4000/login')
+  .then(function (res) {
+      let login = document.querySelector('section')
+      login.innerHTML = res.data;
+    console.log(res.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+
+function carregarMapa(){
+    axios.get('http://localhost:4000/mapa')
+  .then(function(res){
+    let mapa = document.querySelector()
+    mapa.innerHTML = res.data;
+    console.log(res.data);
+  }
+)
+}
+
+// Banner
+
+let banner = document.querySelector('.banner')
